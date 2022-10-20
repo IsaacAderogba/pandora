87bb9a1ff150e5fa5332eaf5c59aefe43f883b0f
Isaac Aderogba <isaacaderogba1@gmail.com>
2022-10-20 17:01:36 +0100

    Add health probe for python service

--- services/actions/src/main.py
+++ services/actions/src/main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
+from src.tasks.health.router import health_router
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(health_router, prefix="/health", tags=["health"])
