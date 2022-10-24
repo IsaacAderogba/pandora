fcaef7027bb9b1961d7132f5ef8b1df7d91efa69
Isaac Aderogba <isaacaderogba1@gmail.com>
2022-10-24 20:37:45 +0100

    Add notion class

--- services/agent/src/libs/notion.ts
+++ services/agent/src/libs/notion.ts
@@ -1,1 +1,23 @@
-// todo
+import { RateLimiterAbstract, RateLimiterMemory } from "rate-limiter-flexible";
+import { Client } from "@notionhq/client";
+
+export interface NotionProps {
+  requestsPerSecond: number;
+}
+
+export class Notion {
+  limiter: RateLimiterAbstract;
+  client: Client;
+
+  constructor({ requestsPerSecond }: NotionProps) {
+    this.limiter = new RateLimiterMemory({
+      points: requestsPerSecond,
+      duration: 1,
+    });
+
+    this.client = new Client({
+      auth: process.env.NOTION_SECRET,
+      notionVersion: "2022-06-28",
+    });
+  }
+}
--- services/agent/src/main.ts
+++ services/agent/src/main.ts
@@ -2,3 +2,4 @@ import path from "path";
 import { Worker } from "worker_threads";
 
 new Worker(path.join(__dirname, "serverWorker.ts"));
+new Worker(path.join(__dirname, "notionSyncWorker.ts"));
