f59cf1d605fa7b95e337ef0c151c8b38397b7218
Isaac Aderogba <isaacaderogba1@gmail.com>
2022-10-28 19:21:37 +0100

    Set up summarization router structure

--- services/actions/src/tasks/summarization/router.py
+++ services/actions/src/tasks/summarization/router.py
@@ -1,9 +1,17 @@
+from typing import List
 from fastapi import APIRouter
 from pydantic import BaseModel
+
+from src.libs.agent.types import Paragraph
 
 summarization_router = APIRouter()
 
 
+class RequestBody(BaseModel):
+    data: List[Paragraph]
+
+
 @summarization_router.post("/extractive")
-async def extractive():
+async def extractive(body: RequestBody):
+    print(body)
     return {"message": "up and running"}
