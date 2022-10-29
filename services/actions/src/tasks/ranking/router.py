49bc6653a955a937a11e055278e3b3cbac6c4c6c
Isaac Aderogba <isaacaderogba1@gmail.com>
2022-10-29 17:26:35 +0100

    Add section rank for sentences

--- services/actions/src/tasks/ranking/router.py
+++ services/actions/src/tasks/ranking/router.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union
 from fastapi import APIRouter
 from pydantic import BaseModel
 
@@ -15,37 +15,54 @@ class RequestBody(BaseModel):
 def rank_documents(documents: List[Document]) -> List[Document]:
     local_documents: List[Document] = []
     global_sections: List[Section] = []
+    document_sentences: List[Sentence] = []
+    document_contexts: List[str] = []
 
     for document in documents:
         local_documents.append(document)
 
         local_sections: List[Section] = []
-        global_sentences: List[Sentence] = []
-        section_sentence_texts: List[List[str]] = []
+        section_sentences: List[Sentence] = []
+        section_contexts: List[str] = []
 
         for section in document["sections"]:
             global_sections.append(section)
             local_sections.append(section)
 
-            local_sentences: List[Sentence] = []
+            sentences: List[Sentence] = []
             for sent in section["sentences"]:
-                global_sentences.append(sent)
-                local_sentences.append(sent)
+                document_sentences.append(sent)
+                section_sentences.append(sent)
+                sentences.append(sent)
 
-            sentence_texts = [sent["text"] for sent in local_sentences]
+            sent_texts = [sent["text"] for sent in sentences]
+            context = " ".join(sent_texts)
+            section_contexts.append(context)
+            set_textrank(sentences, context, sent_texts, "sentence_rank")
 
-            context = " ".join(sentence_texts)
-            local_sent_rank = textrank(context, sentence_texts)
-            for i, sentence_rank in enumerate(local_sent_rank):
-                local_sentences[i]["metadata"] = {}
-                local_sentences[i]["metadata"] = {"sentence_rank": sentence_rank}
+        context = " ".join(section_contexts)
+        document_contexts.append(context)
 
+        sect_sent_texts = [sent["text"] for sent in section_sentences]
+        set_textrank(section_sentences, context, sect_sent_texts, "section_rank")
 
-        
-
-
+    context = " ".join(document_contexts)
 
     return documents
+
+
+def set_textrank(
+    data: Union[List[Document], List[Section], List[Sentence]],
+    context: str,
+    texts: List[str],
+    key: str,
+):
+    ranks = textrank(context, texts)
+    for i, rank in enumerate(ranks):
+        if data[i]["metadata"] is None:
+            data[i]["metadata"] = {}
+            
+        data[i]["metadata"][key] = rank
 
 
 @ranking_router.post("/textrank")
