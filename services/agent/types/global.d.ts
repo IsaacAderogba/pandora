356152ea9cc4b37b95312b219b4c92f79f7f3c63
Isaac Aderogba <isaacaderogba1@gmail.com>
2022-11-01 21:03:34 +0000

    Expose readwise secret to container

--- services/agent/types/global.d.ts
+++ services/agent/types/global.d.ts
@@ -3,6 +3,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NOTION_SECRET: string;
+      READWISE_SECRET: string;
       SENTRY_DSN: string;
       WORKER: string;
     }
