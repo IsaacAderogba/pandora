9449fc329ae7e9b3c8d8c1a25a6a64b33795a198
Isaac Aderogba <isaacaderogba1@gmail.com>
2022-11-12 14:11:56 +0000

    Add methods for scholar api

---
+++ services/agent/src/libs/scholar/client.ts
@@ -0,0 +1,61 @@
+import axios from "axios";
+import { rateLimit, rateLimiter } from "../limiter";
+import {
+  PaginationResult,
+  Paper,
+  PaperDetail,
+  paperDetailFields,
+  paperFields,
+  PaperRecommendationParameters,
+  PaperSearchParameters,
+} from "./types";
+
+@rateLimiter({ duration: 30000, points: 10 })
+class Scholar {
+  client = axios.create({
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Token ${process.env.READWISE_SECRET}`,
+    },
+    baseURL: "https://api.semanticscholar.org",
+  });
+
+  @rateLimit({ points: 1 })
+  async paperSearch(
+    params: PaperSearchParameters
+  ): Promise<PaginationResult<Paper>> {
+    const { data } = await this.client.get<PaginationResult<Paper>>(
+      "/graph/v1/paper/search",
+      {
+        params: { ...params, fields: paperFields },
+      }
+    );
+
+    return data;
+  }
+
+  @rateLimit({ points: 1 })
+  async paperDetails(id: string): Promise<PaperDetail> {
+    const { data } = await this.client.get<PaperDetail>(
+      `/graph/v1/paper/${id}`,
+      { params: { fields: paperDetailFields } }
+    );
+
+    return data;
+  }
+
+  @rateLimit({ points: 1 })
+  async paperRecommendations(
+    id: string,
+    { limit = 10 }: Partial<PaperRecommendationParameters> = {}
+  ): Promise<Paper[]> {
+    const { data } = await this.client.get<{ recommendPapers: Paper[] }>(
+      `/recommendations/v1/papers/forpaper/${id}`,
+      { params: { limit, fields: paperFields } }
+    );
+
+    return data.recommendPapers;
+  }
+}
+
+export const scholar = new Scholar();
--- services/agent/src/libs/scholar/types.ts
+++ services/agent/src/libs/scholar/types.ts
@@ -1,34 +1,66 @@
 export interface Paper {
   paperId: string;
-  externalIds: {
-    ArXiv: string;
-    DBLP: string;
-    PubMedCentral: string;
-  };
   url: string;
   title: string;
   abstract: string;
-  venue: string;
   year: number;
+  fieldsOfStudy: string[];
+  publicationDate: string;
+  authors: { authorId: string; name: string }[];
+}
+
+export const paperFields = [
+  "paperId",
+  "url",
+  "title",
+  "abstract",
+  "year",
+  "fieldsOfStudy",
+  "publicationDate",
+  "authors",
+] as const;
+
+export interface PaperSearchParameters {
+  query: string;
+  offset?: number;
+  limit?: number;
+}
+
+export interface PaperDetail extends Paper {
+  venue: string;
   referenceCount: number;
   citationCount: number;
   influentialCitationCount: number;
-  isOpenAccess: boolean;
-  openAccessPdf: {
-    url: string;
-    status: string;
-  };
-  fieldsOfStudy: string[];
   publicationTypes: string[];
-  publicationDate: string;
   journal: {
     name: string;
     pages: string;
     volume: string;
   };
-  authors: { authorId: string; name: string }[];
   tldr: {
     model: string;
     text: string;
   };
 }
+
+export const paperDetailFields = [
+  ...paperFields,
+  "venue",
+  "referenceCount",
+  "citationCount",
+  "influentialCitationCount",
+  "publicationTypes",
+  "journal",
+  "tldr",
+];
+
+export interface PaperRecommendationParameters {
+  limit?: number;
+}
+
+export type PaginationResult<T> = {
+  total: number;
+  offset: number;
+  next: number;
+  data: T[];
+};
